5834f56e358174ed5eb8c88064da220a9d28675d
Hironobu Kuroda <hironobu.kuroda@ksk-anl.com>
2018-04-04 17:43:29 +0900

    input to output

--- src/main.rs
+++ src/main.rs
@@ -13,39 +13,47 @@ use std::io;
 
 #[derive(Debug, Deserialize, Serialize)]
 #[serde(rename_all = "PascalCase")]
-struct Record<'a> {
+struct Record {
     latitude: f64,
     longitude: f64,
     #[serde(deserialize_with = "csv::invalid_option")]
     population: Option<u64>,
-    city: &'a str,
-    state: &'a str,
+    city: String,
+    state: String,
 }
 
 fn run() -> Result<(), Box<Error>> {
+    let read_file_path = get_first_arg()?;
     let mut wtr = csv::Writer::from_writer(io::stdout());
+    let mut rdr = csv::Reader::from_path(read_file_path)?;
 
-    wtr.serialize(Record {
-        city: "Davidsons Landing",
-        state: "AK",
-        population: None,
-        latitude: 65.2419444,
-        longitude: -165.2716667,        
-    })?;
-    wtr.serialize(Record {
-        city: "Kenai",
-        state: "AK",
-        population: Some(7610),
-        latitude: 60.5544444,
-        longitude: -151.2583333,
-    })?;
-    wtr.serialize(Record {
-        city: "Oakman",
-        state: "AL",
-        population: None,
-        latitude: 33.7133333,
-        longitude: -87.3886111,
-    })?;
+    for result in rdr.deserialize() {
+        let record: Record = result?;
+        wtr.serialize(record)?;
+    }
+
+
+    // wtr.serialize(Record {
+    //     city: "Davidsons Landing",
+    //     state: "AK",
+    //     population: None,
+    //     latitude: 65.2419444,
+    //     longitude: -165.2716667,        
+    // })?;
+    // wtr.serialize(Record {
+    //     city: "Kenai",
+    //     state: "AK",
+    //     population: Some(7610),
+    //     latitude: 60.5544444,
+    //     longitude: -151.2583333,
+    // })?;
+    // wtr.serialize(Record {
+    //     city: "Oakman",
+    //     state: "AL",
+    //     population: None,
+    //     latitude: 33.7133333,
+    //     longitude: -87.3886111,
+    // })?;
 
     wtr.flush()?;
     Ok(())
