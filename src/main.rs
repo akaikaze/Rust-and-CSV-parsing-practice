fc54070e572180577754f9f558df1ef03314fff5
Hironobu Kuroda <hironobu.kuroda@ksk-anl.com>
2018-04-04 17:12:32 +0900

    way of couldnot parse string

--- src/main.rs
+++ src/main.rs
@@ -11,18 +11,14 @@ use std::fs::File;
 use std::process;
 
 #[derive(Debug, Deserialize)]
+#[serde(rename_all = "PascalCase")]
 struct Record {
-    #[serde(rename = "Latitude")]
     latitude: f64,
-    #[serde(rename = "Longitude")]
     longitude: f64,
-    #[serde(rename = "Population")]
+    #[serde(deserialize_with = "csv::invalid_option")]
     population: Option<u64>,
-    #[serde(rename = "City")]
     city: String,
-    #[serde(rename = "State")]
     state: String,
-
 }
 
 fn run() -> Result<(), Box<Error>> {
