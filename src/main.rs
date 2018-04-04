284cd01ebf07c2d79ebc5759d3a8dd34cef11382
Hironobu Kuroda <hironobu.kuroda@ksk-anl.com>
2018-04-04 17:25:23 +0900

    output with serde

--- src/main.rs
+++ src/main.rs
@@ -23,17 +23,12 @@ use std::io;
 // }
 
 fn run() -> Result<(), Box<Error>> {
-    let file_path = get_first_arg()?;
-    let mut wtr = csv::WriterBuilder::new()
-        .delimiter(b'\t')
-        .quote_style(csv::QuoteStyle::NonNumeric)
-        .from_writer(io::stdout());
-        
-    wtr.write_record(&["City", "State", "Population", "Latitude", "Longitude"])?;
-    wtr.write_record(&["Davidsons Landing", "AK", "", "65.2419444", "-165.2716667"])?;
-    wtr.write_record(&["Kenai", "AK", "7610", "60.5544444", "-151.2583333"])?;
-    wtr.write_record(&["Oakman", "AL", "", "33.7133333", "-87.3886111"])?;
+    let mut wtr = csv::Writer::from_writer(io::stdout());
 
+    wtr.serialize(("Davidsons Landing", "AK", None::<u64>, 65.2419444, -165.2716667))?;
+    wtr.serialize(("Kenai", "AK", Some(7610), 60.5544444, -151.2583333))?;
+    wtr.serialize(("Oakman", "AL", None::<u64>, 33.7133333, -87.3886111))?;
+    
     wtr.flush()?;
     Ok(())
 }
