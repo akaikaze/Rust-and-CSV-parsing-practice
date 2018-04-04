65836968ce1ce9bfe70af9c2ea22dae323b8be8e
Hironobu Kuroda <hironobu.kuroda@ksk-anl.com>
2018-04-04 18:41:41 +0900

    performance test with serde struct

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,42 @@
 extern crate csv;
-// extern crate serde;
+extern crate serde;
 
-// #[macro_use]
-// extern crate serde_derive;
+#[macro_use]
+extern crate serde_derive;
 
 use std::env;
 use std::error::Error;
 use std::ffi::OsString;
-// use std::fs::File;
+use std::fs::File;
 use std::process;
 use std::io;
 
-// #[derive(Debug, Deserialize, Serialize)]
-// #[serde(rename_all = "PascalCase")]
-// struct Record {
-//     city: String,
-//     state: String,
-//     latitude: f64,
-//     longitude: f64,
-//     #[serde(deserialize_with = "csv::invalid_option")]
-//     population: Option<u64>,
-// }
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "PascalCase")]
+struct Record<'a> {
+    country: &'a str,
+    city: &'a str,
+    accent_city: &'a str,
+    region: &'a str,
+    population: Option<u64>,
+    latitude: f64,
+    longitude: f64,
+}
 
 fn run() -> Result<u64, Box<Error>> {
 
     let read_file_path = get_first_arg()?;
-    let mut rdr = csv::Reader::from_path(read_file_path)?;
-    let mut record = csv::ByteRecord::new();
-    
+    let file = File::open(read_file_path)?;
+    let mut rdr = csv::Reader::from_reader(file);
+    // let mut rdr = csv::Reader::from_reader(io::stdin());
+    let mut raw_record = csv::StringRecord::new();
+    let headers = rdr.headers()?.clone();    
+
     let mut count = 0;
 
-    while rdr.read_byte_record(&mut record)? {
-        if &record[0] == b"us" && &record[3] == b"MA" {
+    while rdr.read_record(&mut raw_record)? {
+        let record: Record = raw_record.deserialize(Some(&headers))?;
+        if record.country == "us" && record.region == "MA" {
             count += 1;
         }
     }
@@ -47,8 +52,13 @@ fn get_first_arg() -> Result<OsString, Box<Error>> {
 }
 
 fn main() {
-    if let Err(err) = run() {
-        println!("{}", err);
-        process::exit(1);
+    match run() {
+        Ok(count) => {
+            println!("{}", count);
+        }
+        Err(err) => {
+            println!("{}", err);
+            process::exit(1);
+        }
     }
 }
