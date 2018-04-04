abbcf54715b52544cd8a35269f20d6c908289e9e
Hironobu Kuroda <hironobu.kuroda@ksk-anl.com>
2018-04-04 18:11:19 +0900

    performance test use byte_records

--- src/main.rs
+++ src/main.rs
@@ -29,9 +29,9 @@ fn run() -> Result<u64, Box<Error>> {
     
     let mut count = 0;
 
-    for result in rdr.records() {
+    for result in rdr.byte_records() {
         let record = result?;
-        if &record[0] == "us" && &record[3] == "MA" {
+        if &record[0] == b"us" && &record[3] == b"MA" {
             count += 1;
         }
     }
