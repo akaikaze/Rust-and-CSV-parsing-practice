993f03806997916931d406ae6fcc8be9f057a77b
Hironobu Kuroda <hironobu.kuroda@ksk-anl.com>
2018-04-04 16:55:17 +0900

    csv parse by serde

--- src/main.rs
+++ src/main.rs
@@ -6,23 +6,16 @@ use std::ffi::OsString;
 use std::fs::File;
 use std::process;
 
-fn run() -> Result<(), Box<Error>>{
+type Record = (String, String, Option<u64>, f64, f64);
+
+fn run() -> Result<(), Box<Error>> {
     let file_path = get_first_arg()?;
     let mut rdr = csv::Reader::from_path(file_path)?;
-    for result in rdr.records( ){
-        let record = result?;
-        let city = &record[0];
-        let state = &record[1];
-        let pop: Option<u64> = record[2].parse().ok();
-        let latitude: f64 = record[3].parse()?;
-        let longitude: f64 = record[4].parse()?;
-        println!(
-            "city: {:?}, state: {:?}, \
-             pop: {:?}, latitude: {:?}, longitude: {:?}",
-            city, state, pop, latitude, longitude);
-        }
-    let headers = rdr.headers()?;
-    println!("{:?}", headers);
+    for result in rdr.deserialize() {
+
+        let record: Record = result?;
+        println!("{:?}", record);
+    }
     Ok(())
 }
 
