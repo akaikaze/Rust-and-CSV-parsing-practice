4f7ca634f2d5981195bcc761b6287a6e014abd00
Hironobu Kuroda <hironobu.kuroda@ksk-anl.com>
2018-04-04 17:31:57 +0900

    output with serde struct

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 extern crate csv;
 extern crate serde;
 
-// #[macro_use]
-// extern crate serde_derive;
+#[macro_use]
+extern crate serde_derive;
 
 use std::env;
 use std::error::Error;
@@ -11,24 +11,42 @@ use std::fs::File;
 use std::process;
 use std::io;
 
-// #[derive(Debug, Deserialize)]
-// #[serde(rename_all = "PascalCase")]
-// struct Record {
-//     latitude: f64,
-//     longitude: f64,
-//     #[serde(deserialize_with = "csv::invalid_option")]
-//     population: Option<u64>,
-//     city: String,
-//     state: String,
-// }
+#[derive(Debug, Deserialize, Serialize)]
+#[serde(rename_all = "PascalCase")]
+struct Record<'a> {
+    latitude: f64,
+    longitude: f64,
+    #[serde(deserialize_with = "csv::invalid_option")]
+    population: Option<u64>,
+    city: &'a str,
+    state: &'a str,
+}
 
 fn run() -> Result<(), Box<Error>> {
     let mut wtr = csv::Writer::from_writer(io::stdout());
 
-    wtr.serialize(("Davidsons Landing", "AK", None::<u64>, 65.2419444, -165.2716667))?;
-    wtr.serialize(("Kenai", "AK", Some(7610), 60.5544444, -151.2583333))?;
-    wtr.serialize(("Oakman", "AL", None::<u64>, 33.7133333, -87.3886111))?;
-    
+    wtr.serialize(Record {
+        city: "Davidsons Landing",
+        state: "AK",
+        population: None,
+        latitude: 65.2419444,
+        longitude: -165.2716667,        
+    })?;
+    wtr.serialize(Record {
+        city: "Kenai",
+        state: "AK",
+        population: Some(7610),
+        latitude: 60.5544444,
+        longitude: -151.2583333,
+    })?;
+    wtr.serialize(Record {
+        city: "Oakman",
+        state: "AL",
+        population: None,
+        latitude: 33.7133333,
+        longitude: -87.3886111,
+    })?;
+
     wtr.flush()?;
     Ok(())
 }
