d1c142b2a21d3d26eafea97834202b2de64a7d8b
Hironobu Kuroda <hironobu.kuroda@ksk-anl.com>
2018-04-04 18:10:00 +0900

    pefomance test use records

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,42 @@
 extern crate csv;
-extern crate serde;
+// extern crate serde;
 
-#[macro_use]
-extern crate serde_derive;
+// #[macro_use]
+// extern crate serde_derive;
 
 use std::env;
 use std::error::Error;
 use std::ffi::OsString;
-use std::fs::File;
+// use std::fs::File;
 use std::process;
 use std::io;
 
-#[derive(Debug, Deserialize, Serialize)]
-#[serde(rename_all = "PascalCase")]
-struct Record {
-    latitude: f64,
-    longitude: f64,
-    #[serde(deserialize_with = "csv::invalid_option")]
-    population: Option<u64>,
-    city: String,
-    state: String,
-}
+// #[derive(Debug, Deserialize, Serialize)]
+// #[serde(rename_all = "PascalCase")]
+// struct Record {
+//     city: String,
+//     state: String,
+//     latitude: f64,
+//     longitude: f64,
+//     #[serde(deserialize_with = "csv::invalid_option")]
+//     population: Option<u64>,
+// }
 
-fn run() -> Result<(), Box<Error>> {
-    let query = match env::args().nth(2) {
-        None => return Err(From::from("expected 1 argument, but got none")),
-        Some(query) => query,
-    };
+fn run() -> Result<u64, Box<Error>> {
 
     let read_file_path = get_first_arg()?;
-    let mut wtr = csv::Writer::from_writer(io::stdout());
     let mut rdr = csv::Reader::from_path(read_file_path)?;
     
-    wtr.write_record(rdr.byte_headers()?)?;
+    let mut count = 0;
 
-    for result in rdr.byte_records() {
+    for result in rdr.records() {
         let record = result?;
-        if record.iter().any(|field| field == query.as_bytes()) {
-            wtr.write_record(&record)?;
+        if &record[0] == "us" && &record[3] == "MA" {
+            count += 1;
         }
     }
 
-    wtr.flush()?;
-    Ok(())
+    Ok(count)
 }
 
 fn get_first_arg() -> Result<OsString, Box<Error>> {
